a009ff9c58041a743e1d84aa2ec635dbaa22e428
muhammadmuas99-collab <muhammadmuas99@gmail.com>
2026-04-17 00:35:08 +0700

    menambahkan code kedalam dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Halaman Utama (Landing Page)
 Route::get('/', function () {
     return view('Landing_page');
+});
+
+// Halaman Dashboard Admin
+Route::get('/admin/dashboard', function () {
+    return view('Dashboard_admin');
+});
+
+// Halaman Welcome (Bawaan Laravel)
+Route::get('/welcome', function () {
+    return view('welcome');
 });
